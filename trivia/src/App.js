5d3296830f93d7502254553cb81a359985f366b9
Tyler <td20147@gmail.com>
2023-04-07 12:58:32 -0400

    Added title component

--- trivia/src/App.js
+++ trivia/src/App.js
@@ -2,17 +2,22 @@ import './App.css';
 import MyButton from './Buttons.js';
 import { ThemeProvider } from '@emotion/react';
 import theme from "./theme";
+import Title from './Title.js'
 
 function App() { 
   return (
-
-    <div class="buttons">
-      <ThemeProvider theme={theme}>
-        <MyButton />
-        <MyButton />
-        <MyButton />
-        <MyButton />
-      </ThemeProvider>
+    <div class="App">
+      <div class="title">
+        <Title />
+      </div>
+      <div class="buttons">
+        <ThemeProvider theme={theme}>
+          <MyButton />
+          <MyButton />
+          <MyButton />
+          <MyButton />
+        </ThemeProvider>
+      </div>
     </div>
   );
 };
---
+++ trivia/src/Title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+
+function TitleBar() {
+  return (
+    <Typography variant="h1">Trivia Game</Typography>
+  );
+}
+
+export default TitleBar;
